58c1ecd8df45babfe17554f9f7159ff5c51543b0
Bill Dickson <trekbill@gmail.com>
2019-11-14 12:18:43 -0700

    update
    update

--- src/views/ViewMap.vue
+++ src/views/ViewMap.vue
@@ -68,7 +68,10 @@ export default {
       clickedFeatureLayerId: null,
       clickedFeature: null,
       clickedLayer: null,
-      currentTileLayer: null,
+      currentTileLayer: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        maxZoom: 17,
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+      }),
       esriWorldImagery: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
       }),
@@ -160,8 +163,8 @@ export default {
         prefix: 'fas'
       })
       this.map = L.map('lmap').setView(this.mapCenter, this.mapZoom)
-      this.openTopoMap.addTo(this.map)
-      this.currentTileLayer = this.openTopoMap
+      console.log('currentTileLayer', this.currentTileLayer)
+      this.currentTileLayer.addTo(this.map)
       //  interate through the data object and render geoJson
       _.forEach(this.mapData.layers_json, (layer) => {
         let layerId = layer.id
